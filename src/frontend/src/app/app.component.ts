9f5338c12f5ba2f513d4cbc022f946a0a4e441fd
Elias Urrutia <elias-samp@hotmail.com>
2021-10-21 02:32:26 -0300

    v2

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'frontend';
   dateYear: number = new Date().getFullYear();
-  resultado: string = "";
+  resultado : string = '';
 
-  constructor(){}
+  constructor(private Service: AppService){}
 
-}
+  ngOnInit() {
+    this.Upload();
+  }
+
+  Upload() {
+    this.Service.numberBet().subscribe((response)=> {
+      console.log('response', response)
+    }, (err)=> {
+      console.log('error', err);
+    })
+      
+    };
+  }
+
+
